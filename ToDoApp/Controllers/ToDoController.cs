c68a0c068ff7a6cde990235c2141e99e8763b110
Jelle Schräder <jelleschrader@gmail.com>
2019-12-17 09:10:20 +0100

    Add controller, models and angular

---
+++ ToDoApp/Controllers/ToDoController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ToDoApp.Models;
+
+namespace ToDoApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<ToDoItem>> GetAll()
+        {
+            return new List<ToDoItem>() { new ToDoItem("Walk the dog"), new ToDoItem("Dishes"), new ToDoItem("Wash car") };
+        }
+    }
+}
---
+++ ToDoApp/Models/ToDoItem.cs
@@ -0,0 +1,13 @@
+﻿namespace ToDoApp.Models
+{
+    public class ToDoItem
+    {
+        public string Title { get; }
+        public bool Done { get; set; }
+
+        public ToDoItem(string title)
+        {
+            Title = title;
+        }
+    }
+}
